a93a1151cc0678fb10e0974fe8ddc69d7b516293
bernackimark <142451393+bernackimark@users.noreply.github.com>
2023-11-26 00:13:11 +0000

    Various early stage tasks

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Python Developer',
+    'location': 'Bengaluru, India',
+    'salary': '100,000',
+    'currency': 'INR'
+  },
+  {
+    'id': 2,
+    'title': 'Java Developer',
+    'location': 'San Francisco, CA',
+    'salary': '150,000',
+    'currency': 'USD'
+  },
+]
+
 @app.route('/')
 def hello_world():
-  return 'Hello World!'
+  return render_template('home.html', jobs=JOBS, company_name='Bernacki Tech')
+
+@app.route('/api/jobs')
+def get_all_jobs() -> list[dict]:
+  return JOBS
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
